59ff74e3b1517c406f984b5f92404dcb8b73ad44
jk025523 <jk025523@gmail.com>
2018-10-29 19:43:58 +0000

    Delete botSecurity.py
    uploaded in error

--- botSecurity.py
+++
@@ -1,6 +0,0 @@
-consumer_key = 'OPTaaX8FqjPLon75mR9c3lftv'
-consumer_secret ='8sY5lrnz3BKsSIiMOFU2Fw4bDBtaX5NAHa1nyWgrcvfmgTAHer'
-access_token ='1050884840185323520-Xo6jnK18MPp4YOYT23TJ8c1eR5RXj7'
-access_token_secret = 'SybjP9MQyMyD6tTl3Ms5rsgTSupjQ7FHx7IaQxH9B7m5z'
-
-
